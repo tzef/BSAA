d2fab97458edc5c9d1da4db37784d69e578ac6a7
LEE ZHE YU <tzef8220@gmail.com>
2020-08-31 01:19:58 +0800

    Add avatar file to current plan form

--- src/app/page/page.plan.form.component.ts
+++ src/app/page/page.plan.form.component.ts
@@ -46,7 +46,12 @@ import {Observable} from 'rxjs';
                    required [(ngModel)]="formName">
           </div>
           <div class="form-group">
-            <strong>3. 年齡<span style="font-size: 12px; color: darkgray;">(至2018/9/16，實足歲滿13歲至未滿23歲之間)</span>(必填)</strong>
+            <strong>3. 個人照(必填)</strong>
+            <p>JPG檔/大小 300 KB 以內</p>
+            <input mdbInputDirective type="file" class="form-control" (change)="this.formAvatar = $event.target.files[0]">
+          </div>
+          <div class="form-group">
+            <strong>4. 年齡<span style="font-size: 12px; color: darkgray;">(至2020/9/30，實足歲滿13歲至未滿23歲之間)</span>(必填)</strong>
             <input type="number"
                    name="formAge"
                    class="form-control"
@@ -54,7 +59,7 @@ import {Observable} from 'rxjs';
                    required [(ngModel)]="formAge">
           </div>
           <div class="form-group">
-            <strong>4. 連絡地址(必填)</strong>
+            <strong>5. 連絡地址(必填)</strong>
             <input type="text"
                    name="formAddress"
                    class="form-control"
@@ -62,7 +67,7 @@ import {Observable} from 'rxjs';
                    required [(ngModel)]="formAddress">
           </div>
           <div class="form-group">
-            <strong>5. 電話</strong>
+            <strong>6. 電話</strong>
             <input type="text"
                    name="formHomePhone"
                    class="form-control"
@@ -75,37 +80,37 @@ import {Observable} from 'rxjs';
                    [(ngModel)]="formCellPhone">
           </div>
           <div class="form-group">
-            <strong>6. MAIL(必填)</strong>
+            <strong>7. MAIL(必填)</strong>
             <input type="email"
                    name="formMail"
                    class="form-control"
                    placeholder="ex: aa45678@gmail.com"
                    required [(ngModel)]="formMail">
           </div>
+        </div>
+        <div class="col-md-6">
           <div class="form-group">
-            <strong>7. 就讀學校(必填)</strong>
+            <strong>8. 就讀學校(必填)</strong>
             <input type="text"
                    name="formSchool"
                    class="form-control"
                    placeholder="ex: 台北藝術大學美術學系"
                    required [(ngModel)]="formSchool">
           </div>
-        </div>
-        <div class="col-md-6">
           <div class="form-group">
-            <strong>8. 自我介紹(必填)</strong>
+            <strong>9. 自我介紹(必填)</strong>
             <textarea type="text" [(ngModel)]="formIntroduce"
                       placeholder="ex: 熱愛東方文化。覺得悲劇非常美。或許是相由心生，沒表情的時候看起來很憂鬱。生長在桃園市郊，
 這裡一直有一種冷清的感覺，尤其陰雨天，窗外的一切看起來都帶著濃厚的疏離感。畫畫，是我最喜歡的事，能抒發情緒更能帶動成長，如果可以的話，
 我希望就這麼一直畫下去。" class="md-textarea form-control" rows="7"></textarea>
           </div>
           <div class="form-group">
-            <strong>9. 理想<span style="font-size: 12px; color: darkgray;">(可填可不填)</span></strong>
+            <strong>10. 理想<span style="font-size: 12px; color: darkgray;">(可填可不填)</span></strong>
             <textarea type="text" [(ngModel)]="formDream"
                       class="md-textarea form-control" rows="7"></textarea>
           </div>
           <div class="form-group">
-            <strong>10. 作品創作理念<span style="font-size: 12px; color: darkgray;">(300字為限)</span>(必填)</strong>
+            <strong>11. 作品創作理念<span style="font-size: 12px; color: darkgray;">(300字為限)</span>(必填)</strong>
             <textarea type="text" [(ngModel)]="formCreateConcept"
                       placeholder="ex: 當地震、海嘯、火山爆發等天然災害來臨前，均會產生頻率非常低的聲波，即「次聲波」。
   次聲波是指頻率小於20赫茲（人耳能聽見的最小頻率），但是高於氣候造成的氣壓變動的聲波。人耳基本上聽不到次聲波，但是能通過其波壓感受到，
@@ -136,6 +141,7 @@ export class PagePlanFormComponent {
   date = Date();
   year = new Date().getFullYear();
   formFile: HTMLInputElement;
+  formAvatar: HTMLInputElement;
   formVideoUrl = '';
   formProductName = '';
   formProductMaterial = '';
@@ -154,6 +160,7 @@ export class PagePlanFormComponent {
   uploading = false;
   toastFlag = true;
   private _uploadedFileUrl: string;
+  private _uploadedAvatarUrl: string;
   uploadPercent: Observable<string>;
 
   constructor(private database: AngularFireDatabase, private storage: AngularFireStorage, private toastService: ToastService) {
@@ -168,17 +175,11 @@ export class PagePlanFormComponent {
     if (this._uploadedFileUrl !== url) {
       this._uploadedFileUrl = url;
       this.database.object('plan/form/' + this.year + '/' + this.formKey)
-        .update({ fileUrl: url })
+        .update({ fileUrl: url, avatarUrl: this._uploadedAvatarUrl })
         .then(_ => {
           if (this.toastFlag) {
             this.toastFlag = false;
-            const html = `<div>作品: </div><div><img src="${url}"></div><div>影音檔連結: ${this.formVideoUrl}</div>
-<div>作品名稱: ${this.formProductName}</div><div>媒材: ${this.formProductMaterial}</div>
-<div>尺寸: ${this.formProductSize}</div><div>姓名: ${this.formName}</div><div>年齡: ${this.formAge}</div>
-<div>聯絡地址: ${this.formAddress}</div><div>住家電話: ${this.formHomePhone}</div><div>手機號碼: ${this.formCellPhone}</div>
-<div>Email: <a href="mailto:${this.formMail}">${this.formMail}</a></div><div>就讀學校: ${this.formSchool}</div>
-<div>自我介紹: ${this.formIntroduce}</div><div>理想: ${this.formDream}</div><div>創作理念: ${this.formCreateConcept}</div>
-<div>表單送出時間: ${this.date}</div>`;
+            const html = `<div>作品: </div><div><img src="${url}"></div><div>影音檔連結: ${this.formVideoUrl}</div><div>作品名稱: ${this.formProductName}</div><div>媒材: ${this.formProductMaterial}</div><div>尺寸: ${this.formProductSize}</div><div>姓名: ${this.formName}</div><div>個人照: </div><div><img src="${this._uploadedAvatarUrl}"></div><div>年齡: ${this.formAge}</div><div>聯絡地址: ${this.formAddress}</div><div>住家電話: ${this.formHomePhone}</div><div>手機號碼: ${this.formCellPhone}</div><div>Email: <a href="mailto:${this.formMail}">${this.formMail}</a></div><div>就讀學校: ${this.formSchool}</div><div>自我介紹: ${this.formIntroduce}</div><div>理想: ${this.formDream}</div><div>創作理念: ${this.formCreateConcept}</div><div>表單送出時間: ${this.date}</div>`;
             this.database.object('sendBox/' + this.formKey)
               .set({
                 title: '報名本屆炫光_' + this.formName,
@@ -187,9 +188,11 @@ export class PagePlanFormComponent {
               })
               .then(result => {
                 this._uploadedFileUrl = null;
+                this._uploadedAvatarUrl = null;
                 this.uploading = false;
                 this.formKey = null;
                 this.formFile = null;
+                this.formAvatar = null;
                 this.formVideoUrl = '';
                 this.formProductName = '';
                 this.formProductMaterial = '';
@@ -232,6 +235,12 @@ export class PagePlanFormComponent {
       alert('請輸入尺寸');
     } else if (this.formName === '') {
       alert('請輸入姓名');
+    } else if (this.formAvatar == null) {
+      alert('請選擇一張個人照片');
+    } else if (this.formFile != null && this.formFile.type !== 'image/jpeg' && this.formFile.type !== 'image/jpg') {
+      alert('個人照片格式只接收 JPG 檔案');
+    } else if (this.formFile != null && this.formFile.size > 1024 * 300) {
+      alert('個人照片檔案大小必需在 300KB 以內');
     } else if (this.formAge === '') {
       alert('請輸入年齡');
     } else if (Number(this.formAge) < 13 || Number(this.formAge) > 22) {
@@ -269,6 +278,7 @@ export class PagePlanFormComponent {
           dream: this.formDream,
           createConcept: this.formCreateConcept,
           fileUrl: 'fileUploading',
+          avatarUrl: 'fileUploading',
           createdAt: this.date,
         })
         .then(result => {
@@ -285,50 +295,81 @@ export class PagePlanFormComponent {
                     .pipe(delay(1000))
                     .pipe(retry(2))
                     .subscribe(value => {
-                      this.uploadedFileUrl = value;
+                      const fileRef2 = 'plan/form/' + this.year + '/' + result.key + '_Avatar';
+                      const task2 = this.storage.upload(fileRef2, this.formAvatar);
+                      this.uploadPercent = task2.percentageChanges().pipe(delay(1000)).pipe(
+                        map((number2) => {
+                          if (number2 === 100) {
+                            this.storage.ref(fileRef2)
+                              .getDownloadURL()
+                              .pipe(delay(1000))
+                              .pipe(retry(2))
+                              .subscribe(value2 => {
+                                this._uploadedAvatarUrl = value2;
+                                this.uploadedFileUrl = value;
+                              });
+                          }
+                          return (50 + number / 2) + '%';
+                        })
+                      );
+                    });
+                }
+                return (number / 2) + '%';
+              })
+            );
+          } else {
+            const fileRef = 'plan/form/' + this.year + '/' + result.key + '_Avatar';
+            const task = this.storage.upload(fileRef, this.formAvatar);
+            this.uploadPercent = task.percentageChanges().pipe(delay(1000)).pipe(
+              map((number) => {
+                if (number === 100) {
+                  this.storage.ref(fileRef)
+                    .getDownloadURL()
+                    .pipe(delay(1000))
+                    .pipe(retry(2))
+                    .subscribe(value => {
+                      this.database.object('plan/form/' + this.year + '/' + this.formKey)
+                        .update({ avatarUrl: value })
+                        .then(_ => {
+                          if (this.toastFlag) {
+                            this.toastFlag = false;
+                            const html = `<div>作品: </div><div>影音檔連結: ${this.formVideoUrl}</div><div>作品名稱: ${this.formProductName}</div><div>媒材: ${this.formProductMaterial}</div><div>尺寸: ${this.formProductSize}</div><div>姓名: ${this.formName}</div><div>個人照: </div><div><img src="${value}"></div><div>年齡: ${this.formAge}</div><div>聯絡地址: ${this.formAddress}</div><div>住家電話: ${this.formHomePhone}</div><div>手機號碼: ${this.formCellPhone}</div><div>Email: <a href="mailto:${this.formMail}">${this.formMail}</a></div><div>就讀學校: ${this.formSchool}</div><div>自我介紹: ${this.formIntroduce}</div><div>理想: ${this.formDream}</div><div>創作理念: ${this.formCreateConcept}</div><div>表單送出時間: ${this.date}</div>`;
+                            this.database.object('sendBox/' + this.formKey)
+                              .set({
+                                title: '報名本屆炫光_' + this.formName,
+                                html: html,
+                                date: this.date
+                              })
+                              .then(result2 => {
+                                this._uploadedFileUrl = null;
+                                this._uploadedAvatarUrl = null;
+                                this.uploading = false;
+                                this.formKey = null;
+                                this.formFile = null;
+                                this.formAvatar = null;
+                                this.formVideoUrl = '';
+                                this.formProductName = '';
+                                this.formProductMaterial = '';
+                                this.formProductSize = '';
+                                this.formName = '';
+                                this.formAge = '';
+                                this.formAddress = '';
+                                this.formHomePhone = '';
+                                this.formCellPhone = '';
+                                this.formMail = '';
+                                this.formSchool = '';
+                                this.formIntroduce = '';
+                                this.formDream = '';
+                                this.formCreateConcept = '';
+                                this.toastService.success('表單送出成功！');
+                              });
+                          }
+                        });
                     });
                 }
                 return number + '%';
               })
             );
-          } else {
-            if (this.toastFlag) {
-              this.toastFlag = false;
-              const html = `<div>作品: </div><div>影音檔連結: ${this.formVideoUrl}</div>
-<div>作品名稱: ${this.formProductName}</div><div>媒材: ${this.formProductMaterial}</div>
-<div>尺寸: ${this.formProductSize}</div><div>姓名: ${this.formName}</div><div>年齡: ${this.formAge}</div>
-<div>聯絡地址: ${this.formAddress}</div><div>住家電話: ${this.formHomePhone}</div><div>手機號碼: ${this.formCellPhone}</div>
-<div>Email: <a href="mailto:${this.formMail}">${this.formMail}</a></div><div>就讀學校: ${this.formSchool}</div>
-<div>自我介紹: ${this.formIntroduce}</div><div>理想: ${this.formDream}</div><div>創作理念: ${this.formCreateConcept}</div>
-<div>表單送出時間: ${this.date}</div>`;
-              this.database.object('sendBox/' + this.formKey)
-                .set({
-                  title: '報名本屆炫光_' + this.formName,
-                  html: html,
-                  date: this.date
-                })
-                .then(result2 => {
-                  this._uploadedFileUrl = null;
-                  this.uploading = false;
-                  this.formKey = null;
-                  this.formFile = null;
-                  this.formVideoUrl = '';
-                  this.formProductName = '';
-                  this.formProductMaterial = '';
-                  this.formProductSize = '';
-                  this.formName = '';
-                  this.formAge = '';
-                  this.formAddress = '';
-                  this.formHomePhone = '';
-                  this.formCellPhone = '';
-                  this.formMail = '';
-                  this.formSchool = '';
-                  this.formIntroduce = '';
-                  this.formDream = '';
-                  this.formCreateConcept = '';
-                  this.toastService.success('表單送出成功！');
-                });
-            }
           }
         });
     }
